5979b70f0d895c108cf56211ef66416ed88d3108
unknown <nvenkatesh@ucsb.edu>
2022-12-06 15:39:06 -0800

    Updated!

--- SVM.py
+++ SVM.py
@@ -97,6 +97,7 @@ print()
 
 # How does time cost grow as sample_size increases (with a fixed feature_size)?
 
+
 features = 1000
 sampleSizes = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
 times = []
@@ -129,5 +130,45 @@ for i in range(len(sampleSizes)):
 plt.plot(sampleSizes, times)
 plt.title("Time to solve the hard margin SVM problem with a feature size of " + str(features) + " and varying sample sizes")
 plt.xlabel("Sample Size")
+plt.ylabel("Time Taken In Seconds To Solve The Problem")
+plt.show()
+print()
+
+
+# How does time cost grow as feature_size increases (with a fixed sample_size)?
+
+
+samples = 5000
+featureSizes = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+times = []
+
+print("Testing execution time as sample size is fixed and feature size varies:")
+for i in range(len(featureSizes)):
+    X, y = generatePoints(featureSizes[i], samples)
+    t0 = time.time()
+
+    dim = X.shape[1]  # dimensionality
+    num = X.shape[0]  # sample size
+
+    Q = np.eye(dim+1)
+    Q[0, 0] = 0
+    p = np.zeros((dim+1, 1))
+    A = np.array(np.concatenate((y, y * X), axis=1)).astype(float)
+    c = np.ones((num, 1))
+
+    print("Testing feature size = " + str(featureSizes[i]) + ":")
+    sol = solvers.qp(P=matrix(Q), q=matrix(p), G=matrix(-A), h=matrix(-c))
+
+    # Uncomment the following 2 lines if you want to view the results of the solutions. I commented these out because it was flooding the terminal
+    # print("The first value is b* and the rest of the values correspond to w*")
+    # print(sol['x'])
+
+    t1 = time.time()
+    times.append(t1 - t0)
+    print("Time Taken For This Iteration = " + str(t1 - t0) + " seconds")
+
+plt.plot(featureSizes, times)
+plt.title("Time to solve the hard margin SVM problem with a sample size of " + str(samples) + " and varying feature sizes")
+plt.xlabel("Feature Size")
 plt.ylabel("Time Taken In Seconds To Solve The Problem")
 plt.show()
